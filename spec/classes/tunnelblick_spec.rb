a980b54f76681bedb85a458fda8195c063f7b6c5
Romuald du Song <romuald.dusong@deveryware.com>
2014-07-14 17:12:13 +0200

    use version as class parameter

--- spec/classes/tunnelblick_spec.rb
+++ spec/classes/tunnelblick_spec.rb
@@ -1,19 +1,21 @@
 require 'spec_helper'
 
 describe 'tunnelblick' do
+  version = '3.3'
   it do
     should contain_package('Tunnelblick').with({
       :provider => 'appdmg',
-      :source   => 'http://downloads.sourceforge.net/project/tunnelblick/All%20files/Tunnelblick_3.3.dmg',
+      :source   => "http://downloads.sourceforge.net/project/tunnelblick/All%20files/Tunnelblick_#{version}.dmg",
     })
   end
 end
 
 describe 'tunnelblick::beta' do
+  version = '3.4beta22'
   it do
     should contain_package('Tunnelblick').with({
       :provider => 'appdmg',
-      :source   => 'http://downloads.sourceforge.net/project/tunnelblick/All%20files/Tunnelblick_3.4beta22.dmg',
+      :source   => "http://downloads.sourceforge.net/project/tunnelblick/All%20files/Tunnelblick_#{version}.dmg",
     })
   end
 end
